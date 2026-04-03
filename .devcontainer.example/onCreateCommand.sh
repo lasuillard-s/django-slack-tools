ae070bb1d0babd3d045a094bdb6132748a3a85a6
Yuchan Lee <lasuillard@gmail.com>
2026-04-03 08:31:14 +0000

    chore: update devcontainer configuration

--- .devcontainer.example/onCreateCommand.sh
+++ .devcontainer.example/onCreateCommand.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+pipx install pre-commit
+pipx install uv
+
 # Add ngrok key
 curl -fsSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc \
     && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
@@ -7,5 +10,3 @@ curl -fsSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/tr
 sudo apt-get update && sudo apt-get install -y \
     gettext \
     ngrok
-
-pipx install uv
